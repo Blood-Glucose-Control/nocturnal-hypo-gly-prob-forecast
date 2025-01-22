2ed2160017889f4b4af10c7ad1e3231b0289789e
Christopher <110344005+RobotPsychologist@users.noreply.github.com>
2025-01-22 13:49:38 -0500

    Kaggle data download script (#20)

---
+++ scripts/data_downloads/kaggle_data_download.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+
+# Define the target directory
+TARGET_DIR="src/data/kaggle_brisT1D"
+
+# Create the target directory if it doesn't exist
+mkdir -p $TARGET_DIR
+
+# Download the data from the Kaggle competition
+kaggle competitions download -c brist1d -p $TARGET_DIR
+
+# Unzip the downloaded data into the target directory
+unzip "$TARGET_DIR/*.zip" -d $TARGET_DIR
+
+# Remove the zip file after extraction
+rm $TARGET_DIR/*.zip
