cefba00420e59b172ad4766daae36c924b2bd2a7
Phiruby <vilohithrao@gmail.com>
2025-01-26 18:22:53 -0500

    Basic data cleaning for brist1d

--- src/data/data_cleaner.py
+++ src/data/data_cleaner.py
@@ -3,7 +3,10 @@ import pandas as pd
 
 # Simulate insulin on board
 # Convert each row to a single df
-def clean_data(data: pd.DataFrame) -> pd.DataFrame:
+def clean_data(
+        data: pd.DataFrame,
+        data_source_name="kaggle_brisT1D"
+    ) -> pd.DataFrame:
     # keep_columns = [
     #     "id",
     #     "p_num",
@@ -16,8 +19,26 @@ def clean_data(data: pd.DataFrame) -> pd.DataFrame:
     #     "cals",
     #     "activity",
     # ]
+
+    if (data_source_name == "kaggle_brisT1D"):
+        # modifies in place
+        _clean_bris_data(data)
+        return
+
     pass
 
+def _clean_bris_data(data: pd.DataFrame):
+    '''
+    Cleans the bris1TD Kaggle data with the following transformations:
+        1. Deletes columns of historic data (eg: bg-5:55, ..., activity-5:55, ...) --> but does not remove -0:00 timestamp
+    Args:
+        data: the df for the Bris1TD dataset
+    Mutations:
+        Modifies the data in place
+    '''
+    # get columns with bg- or activity-
+    columns_to_drop = [col for col in data.columns if (('activity' in col or 'bg' in col or 'cals' in col or 'insulin' in col) and '-' in col and not col.endswith('-0:00'))]
+    data.drop(columns=columns_to_drop, inplace=True)
 
 def melt_data(data: pd.DataFrame, id_vars: list, value_vars: list) -> pd.DataFrame:
     """
