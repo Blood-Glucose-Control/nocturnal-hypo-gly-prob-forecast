367a551b8c8523068450d4c0763c9165fd15af63
Vilo <89655335+Phiruby@users.noreply.github.com>
2025-03-19 03:49:33 -0400

    Data Smoothing techniques (#162)
    Co-authored-by: Christopher <110344005+RobotPsychologist@users.noreply.github.com>

--- src/data/data_transforms.py
+++ src/data/data_transforms.py
@@ -10,6 +10,7 @@ from src.data.carb_model.constants import (
     COB_COL,
     CARB_AVAIL_COL,
 )
+from src.data.wavelet_transformer import WaveletTransformer
 from src.data.insulin_model.constants import IOB_COL, INSULIN_AVAIL_COL
 from src.data.insulin_model.insulin_model import (
     calculate_insulin_availability_and_iob_single_delivery,
@@ -278,4 +279,73 @@ def ensure_regular_time_intervals(df: pd.DataFrame) -> pd.DataFrame:
     # Combine all reindexed patient data
     result_df = pd.concat(reindexed_dfs)
 
+    return result_df
+
+
+def apply_wavelet_transform(
+    df: pd.DataFrame,
+    wavelet="sym16",
+    wavelet_window=36,
+    patient_identifying_col="p_num",
+    bgl_column="bg-0:00",
+) -> pd.DataFrame:
+    """
+    Applies wavelet transform on each patient's data
+    Separately uses the WaveletTransformer per patient
+
+    Args:
+        df: the input dataframe containing the cumulative data
+        wavelet: the wavelet to use. See WaveletTransformer for more info
+        wavelet_window: window size for the transform. Also see WaveletTransformer
+        patient_identifying_col: the column name that distinguishes each patient's data
+        bgl_column: the column to apply this over
+    """
+    # list to collect the transformed data for each patient
+    transformed_data = []
+
+    # group by patient. separately process them
+    for patient_id, patient_data in df.groupby(patient_identifying_col):
+        bgl_series = patient_data[bgl_column]
+        # Initialize the WaveletTransformer for the current patient
+        transformer = WaveletTransformer(window_len=wavelet_window, wavelet=wavelet)
+
+        transformed_bgl = transformer.fit_transform(bgl_series)
+
+        patient_data[bgl_column] = transformed_bgl
+
+        # adds new col to idntify the patient
+        patient_data[patient_identifying_col] = patient_id
+
+        # Add the transformed data to the list
+        transformed_data.append(patient_data)
+
+    result_df = pd.concat(transformed_data, axis=0)
+
+    # Sort the result DataFrame by the original index to preserve the row order
+    result_df = result_df.sort_index()
+
+    return result_df
+
+
+def take_moving_average(
+    df: pd.DataFrame, window_size: int = 36, bg_col="bg-0:00"
+) -> pd.DataFrame:
+    """
+    Takes the moving average of the dataframe over bg_col
+
+    Args:
+        df: the dataframe
+        window_size: the moving average window size
+        bg_col: the column take moving average over
+    """
+    if bg_col not in df.columns:
+        raise ValueError(f"Column '{bg_col}' not found in the DataFrame")
+
+    # Create a copy to avoid modifying the original
+    result_df = df.copy()
+
+    # Apply moving average
+    result_df[bg_col] = (
+        result_df[bg_col].rolling(window=window_size, min_periods=1).mean()
+    )
     return result_df
