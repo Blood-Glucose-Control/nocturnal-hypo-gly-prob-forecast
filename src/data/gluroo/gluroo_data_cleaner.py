d96a671b9e039a1add7f42342bda22892a1d6baa
Tony Chan <88695195+Tony911029@users.noreply.github.com>
2025-03-25 04:53:44 -0400

    Migrate some code from meal id (#185)
    Co-authored-by: Tony Chan <tony.chan@sciteline.com>
    Co-authored-by: Christopher <110344005+RobotPsychologist@users.noreply.github.com>

--- src/data/gluroo/gluroo_data_cleaner.py
+++
@@ -1,7 +0,0 @@
-"""Data cleaning functions for the GLUROO dataset."""
-
-import pandas as pd
-
-
-def clean_gluroo_data(data: pd.DataFrame) -> pd.DataFrame:
-    pass
--- src/data/gluroo/gluroo_data_loader.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-
-def _clean_gluroo_train_data(data: pd.DataFrame) -> pd.DataFrame:
-    """
-    Cleans the GLUROO dataset.
-    """
