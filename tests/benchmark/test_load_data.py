353d52ee446cfee75443e2db2f77f715bb9787d9
RobotPsychologist <cjr.robot.psychologist@gmail.com>
2025-09-09 02:34:36 +0000

    Removing test_load_data.py, not a good idea for a test

--- tests/benchmark/test_load_data.py
+++ tests/benchmark/test_load_data.py
@@ -1,37 +1,37 @@
-from typing import cast
+# from typing import cast
 
-import pandas as pd
+# import pandas as pd
 
-from src.data.diabetes_datasets.data_loader import get_loader
-
-
-def test_brist1d_load_data_with_missing_values():
-    # This test checks that it works even if data is cached
-    kaggle_loader = get_loader(
-        data_source_name="kaggle_brisT1D", dataset_type="train", use_cached=True
-    )
-    assert kaggle_loader is not None
-    data = kaggle_loader.processed_data
-    data = cast(pd.DataFrame, data)
-    assert len(data["p01"]) > 0
-    assert len(data["p01"].columns) > 0
+# from src.data.diabetes_datasets.data_loader import get_loader
 
 
-def test_brist1d_keep_columns():
-    kaggle_loader = get_loader(
-        data_source_name="kaggle_brisT1D",
-        dataset_type="train",
-        keep_columns=["bg_mM", "p_num", "datetime"],
-        use_cached=True,
-    )
-    assert kaggle_loader is not None
-    data = kaggle_loader.processed_data
-    data = cast(pd.DataFrame, data)
-    assert len(data["p01"]) > 0
-    assert len(data["p01"].columns) == 2
-    assert "bg_mM" in data["p01"].columns
-    assert "p_num" in data["p01"].columns
-    assert "datetime" == data["p01"].index.name
+# def test_brist1d_load_data_with_missing_values():
+#     # This test checks that it works even if data is cached
+#     kaggle_loader = get_loader(
+#         data_source_name="kaggle_brisT1D", dataset_type="train", use_cached=True
+#     )
+#     assert kaggle_loader is not None
+#     data = kaggle_loader.processed_data
+#     data = cast(pd.DataFrame, data)
+#     assert len(data["p01"]) > 0
+#     assert len(data["p01"].columns) > 0
+
+
+# def test_brist1d_keep_columns():
+#     kaggle_loader = get_loader(
+#         data_source_name="kaggle_brisT1D",
+#         dataset_type="train",
+#         keep_columns=["bg_mM", "p_num", "datetime"],
+#         use_cached=True,
+#     )
+#     assert kaggle_loader is not None
+#     data = kaggle_loader.processed_data
+#     data = cast(pd.DataFrame, data)
+#     assert len(data["p01"]) > 0
+#     assert len(data["p01"].columns) == 2
+#     assert "bg_mM" in data["p01"].columns
+#     assert "p_num" in data["p01"].columns
+#     assert "datetime" == data["p01"].index.name
 
 
 # NOTE: can be added later
