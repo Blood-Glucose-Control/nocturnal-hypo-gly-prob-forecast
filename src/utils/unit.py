c1afaef76c61a9f7e933aaba11236ad3887a00c9
Tony Chan <tony.chan@sciteline.com>
2025-11-03 19:41:32 +0000

    minor fix

--- src/utils/unit.py
+++ src/utils/unit.py
@@ -2,4 +2,4 @@ import pandas as pd
 
 
 def mg_dl_to_mmol_l(df: pd.DataFrame, bgl_col: str = "bg-0:00") -> pd.DataFrame:
-    return (df[bgl_col] / 18.0).round(2)
+    return (df[bgl_col] / 18.018).round(2)
